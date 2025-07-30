363e3153ffcea85fc95e866b6de1adf95a35264e
Purwa <s.purwarajput@gmail.com>
2025-07-31 00:58:13 +0530

    adding functionalities

---
+++ client/src/components/AiTools.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AiTools = () => {
+  return (
+    <div className='px-4 sm:px-20 xl:px-32 my-24'>
+      <div className='text-center'>
+        <h2 className='text-slate-700 text-[42px] font-semibold'>Powerful AI tools</h2>
+        <p className='text-gray-500 max-w-lg mx-auto'>A complete AI-powered suite to help you ideate, polish, and perfect your content like never before.</p>
+      </div>
+    </div>
+  )
+}
+
+export default AiTools
